2810ae18eba4c419f202943715800e73b7dc0e87
Thumin <96730005+Thu-Min@users.noreply.github.com>
2025-02-17 11:15:03 +0700

    feat: migration for chat room

--- chat/models.py
+++ chat/models.py
@@ -24,6 +24,22 @@ class User(AbstractBaseUser):
     def __str__(self):
         return self.username
     
+class Chat_Room(models.Model):
+    type = models.CharField(max_length=255)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.type
+    
+class Chat_Room_Member(models.Model):
+    chatroom_id = models.ForeignKey(Chat_Room, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    role = models.CharField(max_length=255)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return f"{self.chatroom_id.type}: {self.user_id.username}"
+
 class Message(models.Model):
     sender = models.ForeignKey(User, related_name='sender', on_delete=models.CASCADE)
     content = models.TextField()
